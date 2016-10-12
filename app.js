347d0fa9e3bca1b518a365b6b5327db2705abb48
hc <hucong929@163.com>
2016-10-13 01:48:01 +0800

    pspgbhu

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var path = require('path');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', function (req, res) {
+app.get('/fsm', function (req, res) {
   res.render('index');
 })
 
