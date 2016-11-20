981384ea65ec906b803030aa48c4f5fbbda4e0f0
pspgbhu <brotherchun001@gmail.com>
2016-11-20 15:50:21 +0800

    添加功能： 可自定义像素

--- js/fsm.js
+++ js/fsm.js
@@ -3,6 +3,12 @@ window.onload = function () {
     return selector ? document.querySelector(selector) : selector;
   };
 
+  var InforOfCanvas = function (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+  var inforOfCanvas = new InforOfCanvas(0, 0);
+
   var canvas  = $('#mycanvas'),
       input   = $('#upImg'),
       infor   = $('#infor');
@@ -11,6 +17,8 @@ window.onload = function () {
       heigth  = 0,// 单张图片高度
       src     = [],// 储存本地图片url
       images  = [];// img对象合集
+
+
 
   // 获取图片url
   input.addEventListener('change',readerSrc,false);
@@ -42,17 +50,34 @@ window.onload = function () {
 
   // 输出图片相关信息
   function showInfor() {
+
     images[0].onload = function () {
+
       width = this.naturalWidth;  // 单张图片宽度
       height = this.naturalHeight; // 单张图片高度
+
       var line = src.length < 10 ? src.length : 10, // 列数
       row  = Math.ceil(src.length / 10); // 行数
+
+      // set value in instance of inforOfcanvas
+      inforOfCanvas.width = width * line;
+      inforOfCanvas.height = height * row
+
       // 画布像素信息
-      infor.innerHTML += '<li>雪碧图宽度：'+ (width * line) +'px</li>' +
-      '<li>雪碧图高度：'+ (height * row) +'px</li>';
+      // infor.innerHTML += '<li>雪碧图宽度：'+ (inforOfCanvas.width) +'px</li>' +
+      // '<li>雪碧图高度：'+ (inforOfCanvas.height) +'px</li>';
+
+      $('#input-width').value = inforOfCanvas.width;
+      $('#input-height').value = inforOfCanvas.height;
     }
+
     $('#make-container').classList.remove('hide');// show button of maker
     $('#infor-container').classList.remove('hide');// show informations container
+    settingsShow();
+  }
+
+  // show items of setttings
+  function settingsShow() {
     $('#settings-container').classList.remove('hide');// show settings container
   }
 
@@ -80,16 +105,23 @@ window.onload = function () {
   $('#makeImage').addEventListener('click',makeImage,false)
 
   function makeImage() {
+
     var line = src.length < 10 ? src.length: 10,
         row= Math.ceil(src.length / 10);
+
     // 是否自定义像素
     if($('#input-width').value && $('#input-height').value){
+
       canvas.width = $('#input-width').value;
       canvas.height = $('#input-height').value;
+
+
     } else { // 使用默认像素
-      canvas.width  = width * line;
-      canvas.height = height * row;
+
+      canvas.width  = inforOfCanvas.width;
+      canvas.height = inforOfCanvas.height;
     }
+
     // 设置画布展现比例(该比例不影响最终生成图像结果)
     canvas.style.height = parseInt($('.main-canvas').offsetWidth) * canvas.height / canvas.width + 'px';
     // console.log(getStyle(canvas, 'width') + ',' + canvas.height + ', ' + canvas.width );
@@ -99,14 +131,25 @@ window.onload = function () {
 
   // canvas绘制图像
   function drawCanvas() {
+    var line = src.length < 10 ? src.length: 10,
+        row= Math.ceil(src.length / 10);
+
+    width = $('#input-width').value / line;
+    height = $('#input-height').value / row;
+    // console.log(width, height)
+
     for(var n = 0; n < images.length; n++){
       var w = n % 10;
       var h = Math.floor(n / 10);
-      console.log(w + ',' + h);
+      // console.log(w + ',' + h);
+      // console.log(width * w + ',' + height * h);
       ctx = canvas.getContext('2d');
-      ctx.drawImage(images[n], width*w, height *h);
+      ctx.drawImage(images[n], width * w, height * h, width, height);
     }
     n = 0;
+
+
+
     canvas.classList.remove('hide');
   }
 
